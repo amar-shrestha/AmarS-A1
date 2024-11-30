5e1e9e5d7cfe8d31c7396c962e420e2ec79cb4ea
amar-shrestha <ashrestha.ktm@gmail.com>
2024-11-30 23:11:10 +1100

    Added clinicLocation attribute toGP.

--- GeneralPractitioner.java
+++ GeneralPractitioner.java
@@ -1,3 +1,6 @@
+// Child Class: GeneralPractitioner
 public class GeneralPractitioner extends HealthProfessional {
-    
+    private String clinicLocation;          // attribute for GP
+
+    //
 }
