9ec5825841e5df7619a65872ba9867f3cbd60f0c
amar-shrestha <ashrestha.ktm@gmail.com>
2024-11-30 22:50:15 +1100

    Added default constructor

--- HealthProfessional.java
+++ HealthProfessional.java
@@ -2,4 +2,11 @@ public class HealthProfessional {
     private int id;                 // Unique ID for health professional
     private String name;            // Name of health professional
     private String specialisation;  // Specialisation 
-}
+
+    // Default constructor
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "Unknown";
+        this.specialisation = "General";
+    }
+    
