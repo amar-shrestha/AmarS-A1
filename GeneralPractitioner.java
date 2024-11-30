8a388b7a188653ec072987b25364bc0ca18f29cb
amar-shrestha <ashrestha.ktm@gmail.com>
2024-11-30 23:12:32 +1100

    Added default constructor to GP.

--- GeneralPractitioner.java
+++ GeneralPractitioner.java
@@ -2,5 +2,9 @@
 public class GeneralPractitioner extends HealthProfessional {
     private String clinicLocation;          // attribute for GP
 
-    //
+    // Default constructor
+    public GeneralPractitioner() {
+        super();            // Calls base class default constructor
+        this.clinicLocation = "Not specified";
+    }
 }
