fee1c96d4ff87f6d380863f10821fcda46a95447
amar-shrestha <ashrestha.ktm@gmail.com>
2024-11-30 23:33:38 +1100

    Declared attributes for Appointment class.

---
+++ Appointment.java
@@ -0,0 +1,6 @@
+public class Appointment {
+    private String patientName;         // Name of patient
+    private String patientPhone;        // Patient's phone
+    private String timeSlot;            // Preferred time
+    private HealthProfessional doctor;  // Selected health professional 
+}
