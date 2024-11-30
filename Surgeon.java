f140c87886b46a5cf3dcfdffe14d6f22888c1244
amar-shrestha <ashrestha.ktm@gmail.com>
2024-11-30 23:21:49 +1100

    Added operatingTheater attribute to Surgeon.

--- Surgeon.java
+++ Surgeon.java
@@ -1,4 +1,6 @@
 // Child Class: Surgeon
 public class Surgeon extends HealthProfessional {
-       
+    private String operationTheater;         // Additonal attribute for Surgeons
+
+    // Default constructor
 }
