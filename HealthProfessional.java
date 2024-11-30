ac28c89bd14e3f40647d53caf9f95ed473dbdf2c
amar-shrestha <ashrestha.ktm@gmail.com>
2024-11-30 22:40:06 +1100

    Declared attributes for HealthProfessional base class

---
+++ HealthProfessional.java
@@ -0,0 +1,5 @@
+public class HealthProfessional {
+    private int id;                 // Unique ID for health professional
+    private String name;            // Name of health professional
+    private String specialisation;  // Specialisation 
+}
