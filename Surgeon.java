e9ba711c50f9e15af1a6a3644d4d5aa92e4b6184
amar-shrestha <ashrestha.ktm@gmail.com>
2024-11-30 23:22:58 +1100

    Added default constructor to Surgeon.

--- Surgeon.java
+++ Surgeon.java
@@ -3,4 +3,8 @@ public class Surgeon extends HealthProfessional {
     private String operationTheater;         // Additonal attribute for Surgeons
 
     // Default constructor
+    public Surgeon () {
+        super();            // Call base class default constructor
+        this.operationTheater = "Not assigned";
+    }
 }
