aff99d1b877610f64f7236b58ed01180d1493454
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-01 22:07:23 +1100

    Printing all appointments

--- Assessmentone.java
+++ Assessmentone.java
@@ -12,5 +12,8 @@ public class Assessmentone {
         system.createAppointment(new Appointment ("Rajesh Kunwar", "0405000000", "09:00 AM", gp1));
         system.createAppointment(new Appointment ("Anju Sapkota", "0422000001", "11:00 AM", gp2));
         system.createAppointment(new Appointment ("Anuska Karki", "0424000002", "11:30 AM", surgeon1));
-    
-}
+
+        // Print all appointments
+        System.out.println ("Printing all appointments...");
+        system.printExistingAppointmetns();
+    }
