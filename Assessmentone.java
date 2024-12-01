e23eb6c079279bb9843699a738b848bac7227e96
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-01 21:43:58 +1100

    Created health professional objects
    Created health professional objects in main method.

---
+++ Assessmentone.java
@@ -0,0 +1,8 @@
+public class Assessmentone {
+    public static void main (String[] args) {
+        GeneralPractitioner gp1 = new GeneralPractitioner(1, "Dr. Amar", "Family", "Granville");
+        GeneralPractitioner gp2 = new GeneralPractitioner(2, "Dr. Rachana", "Pediatrics", "Rockdale");
+        Surgeon surgeon1 = new Surgeon(3, "Dr. Aadrik", "Neurosurgery", "OT 1");
+    }
+    
+}
