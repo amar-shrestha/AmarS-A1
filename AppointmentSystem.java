58444e290fb09968a0f407e08aa3b6fc71a2f85c
amar-shrestha <ashrestha.ktm@gmail.com>
2024-12-01 21:25:18 +1100

    Create new appointment method
    Method added

--- AppointmentSystem.java
+++ AppointmentSystem.java
@@ -4,4 +4,14 @@ import java.util.ArrayList;
 public class AppointmentSystem {
     private ArrayList<Appointment> appointments = new ArrayList<>();
 
+    // Methodd to create a new appointment
+    public void createAppointment (Appointment appointment) {
+        if (appointment !=null) {
+            appointments.add(appointment);
+            System.out.println ("Appointment created sucessfully!");
+        } else {
+            System.out.println ("Invalid appointment details.");
+        }
+    }
 }
+
